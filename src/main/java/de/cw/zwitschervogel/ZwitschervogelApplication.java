146de96c14a02255ba593baa3c8c2f2374d6c652
Sebastian Thiems <sthiems@sieber-riedl.de>
2019-04-22 16:42:18 +0200

    Init commit backend

---
+++ src/main/java/de/cw/zwitschervogel/ZwitschervogelApplication.java
@@ -0,0 +1,13 @@
+package de.cw.zwitschervogel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwitschervogelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZwitschervogelApplication.class, args);
+  }
+
+}
