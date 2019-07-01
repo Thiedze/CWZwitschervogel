2977bcea97588b55cd67ef832b53559fb6f82ab2
Sebastian Thiems <sthiems@sieber-riedl.de>
2019-07-01 20:10:35 +0200

    Stage 1

--- server/src/main/java/de/cw/zwitschervogel/server/repository/TweetRepository.java
+++ server/src/main/java/de/cw/zwitschervogel/server/repository/TweetRepository.java
@@ -1,13 +1,10 @@
 package de.cw.zwitschervogel.server.repository;
 
 import de.cw.zwitschervogel.server.domain.Tweet;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-
-  List<Tweet> findAllByOrderByCreatedDesc();
 
 }
